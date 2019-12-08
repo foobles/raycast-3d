5fdf02e3f3422f48d0b8788d20937804093fea52
foobles <buzdav2@gmail.com>
2019-12-08 11:12:21 -0600

    added transparency

--- scene.py
+++ scene.py
@@ -1,16 +1,16 @@
-from collections import namedtuple
 import math
 import pygame as pg
 
 
-RayHitInfo = namedtuple("RayHitInfo", [
-    "hit",
-    "distance",
-    "horizontal",
-    "wall_x_rat"])
+class RayHitInfo:
+    def __init__(self, hit, distance, horizontal, wall_x_rat):
+        self.hit = hit
+        self.distance = distance
+        self.horizontal = horizontal
+        self.wall_x_rat = wall_x_rat
 
 
-def _get_ray_hit_info(pos, ray, world):
+def _get_ray_hit_info(pos, ray, world, textures):
     pos_x, pos_y = pos
     ray_x, ray_y = ray
     tile_x, tile_y = int(pos_x), int(pos_y)
@@ -35,8 +35,9 @@ def _get_ray_hit_info(pos, ray, world):
         if ray_y > 0
         else (pos_y - tile_y))
 
+    ret = []
     hit = 0
-    while hit == 0:
+    while hit == 0 or textures[hit-1].transparent:
         if x_dist_moved < y_dist_moved:
             x_dist_moved += x_tile_dist
             tile_x += x_step
@@ -46,14 +47,16 @@ def _get_ray_hit_info(pos, ray, world):
             tile_y += y_step
             horizontal = True
         hit = world[tile_x, tile_y]
-    if horizontal:
-        distance = (tile_y - pos_y + (1-y_step) / 2) / ray_y
-        wall_x_rat = abs(pos_x + ray_x * distance) % 1
-    else:
-        distance = (tile_x - pos_x + (1-x_step) / 2) / ray_x
-        wall_x_rat = abs(pos_y + ray_y * distance) % 1
+        if hit > 0:
+            if horizontal:
+                distance = (tile_y - pos_y + (1-y_step) / 2) / ray_y
+                wall_x_rat = abs(pos_x + ray_x * distance) % 1
+            else:
+                distance = (tile_x - pos_x + (1-x_step) / 2) / ray_x
+                wall_x_rat = abs(pos_y + ray_y * distance) % 1
+            ret.insert(0, RayHitInfo(hit, distance, horizontal, wall_x_rat))
 
-    return RayHitInfo(hit, distance, horizontal, wall_x_rat)
+    return ret
 
 
 def _get_column(texture, x_rat, height):
@@ -78,14 +81,14 @@ class Scene:
         dark.set_alpha(0.3 * 256)
 
         for x, ray in self.camera.rays(surface.get_width()):
-            ray_info = _get_ray_hit_info(self.camera.pos, ray, self.world)
-            height = int(surface_height / ray_info.distance)
-            column = _get_column(
-                textures[ray_info.hit - 1].surface,
-                ray_info.wall_x_rat,
-                height)
+            for ray_info in _get_ray_hit_info(self.camera.pos, ray, self.world, textures):
+                height = int(surface_height / ray_info.distance)
+                column = _get_column(
+                    textures[ray_info.hit - 1].surface,
+                    ray_info.wall_x_rat,
+                    height)
 
-            surface.blit(column, (x, surface_height / 2 - height / 2))
+                surface.blit(column, (x, surface_height / 2 - height / 2))
 
-            if ray_info.horizontal:
-                surface.blit(dark, (x, 0))
+                if ray_info.horizontal:
+                    surface.blit(dark, (x, 0))
